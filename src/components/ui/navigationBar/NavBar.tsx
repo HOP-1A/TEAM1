59008b571dc3db2dd700d8df5900d378e3b28e10
24HP1005 24HP1005 <24HP1005@admins-iMac.local>
2025-03-25 19:02:32 +0800

    dialog

--- src/components/ui/navigationBar/NavBar.tsx
+++ src/components/ui/navigationBar/NavBar.tsx
@@ -14,13 +14,13 @@ import {
 } from "lucide-react";
 
 import {
-  ClerkProvider,
   SignInButton,
   SignUpButton,
   SignedIn,
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { Nunito } from "next/font/google";
 import Link from "next/link";
@@ -141,7 +141,29 @@ export default function NavBar() {
                 id="rightNavigationIcons"
                 className="flex gap-[16px] text-rose-500 "
               >
-                <button className="flex justify-center bg-slate-200 p-2 rounded-full h-[40px] w-[40px]">
+                <button className="flex justify-center font-3 bg-slate-100 rounded-full h-[40px] w-[40px]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    className="w-4 px;color:#ff3467"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      d="M416 192c0-88.4-93.1-160-208-160S0 103.6 0 192c0 34.3 14.1 65.9 38 92-13.4 30.2-35.5 54.2-35.8
+                    54.5-2.2 2.3-2.8 5.7-1.5 8.7S4.8 352 8 352c36.6 0 66.9-12.3 88.7-25 32.2 15.7 70.3 25 111.3 25 114.9 0
+                    208-71.6 208-160zm122 220c23.9-26 38-57.7 38-92 0-66.9-53.5-124.2-129.3-148.1.9 6.6 1.3 13.3 1.3 20.1 0
+                    105.9-107.7 192-240 192-10.8 0-21.3-.8-31.7-1.9C207.8 439.6 281.8 480 368 480c41 0 79.1-9.2 111.3-25 21.8
+                    12.7 52.1 25 88.7 25 3.2 0 6.1-1.9 7.3-4.8 1.3-2.9.7-6.3-1.5-8.7-.3-.3-22.4-24.2-35.8-54.5z"
+                    />
+                  </svg>
+                </button>
+                <button className="bg-slate-200 p-2 rounded-full h-[40px] w-[40px]">
+                  <Heart className="h-4 fill-rose-500" />
+                </button>
+                <button
+                  onClick={() => router.push(`/cart`)}
+                  className="flex justify-center bg-slate-200 p-2 rounded-full h-[40px] w-[40px]"
+                >
                   <svg
                     viewBox="0 0 15 18"
                     fill="none"
